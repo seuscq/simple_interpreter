04d5b8e3ea730af5e04cf66a51f44cf2f6e389eb
nobody <nobody>
2019-12-11 16:07:20 +0000

    Second Commit

--- calc2.py
+++ calc2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' first phase of this compiler is to evaluate '1+2' '''
 # grammar for this language:
+# 
 # expr => INTEGER op INTEGER
 # op => PLUS | MINUS
 
@@ -16,19 +17,50 @@ class Lexer:
         self.text = text
         self.pos = self.text[0]
 
+    def advance():
+        self.pos++
+
     def get_next_token(self):
-        if 
-        return Token()
+        if self.pos > len(self.text) - 1:
+            return Token(EOF, None)
+
+        if self.text[self.pos].isdigit():
+            d = self.text[self.pos]
+            self.advance()
+            return Token(INTEGER, d)
+        if self.text[self.pos].
 
 class Parser:
     def __init__(self, lexer):
         self.lexer = lexer
         self.current_token = self.lexer.get_next_token()
 
-    def expr(self):
-        token = self.lexer
-        
+    def error(self):
+        raise Exception('syntax error')
 
+    def eat(self, type):
+        if self.current_token.type == type:
+            self.current_token = self.lexer.get_next_token()
+        else
+            self.error()
+
+    def expr(self):
+        token = self.current_token
+        if token.type == INTEGER:
+            self.eat(INTEGER)
+            self.op()
+            self.eat(INTEGER)
+        else
+            self.error()
+            
+    def op(self):
+        token = self.current_token
+        if token.type == PLUS:
+            self.eat(PLUS)
+        elif token.type == MINUS:
+            self.eat(MINUS)
+        else:
+            self.error()
 
 class Interpreter:
     def __init__(self, parser):
